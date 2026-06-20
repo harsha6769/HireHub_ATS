f38be45f8998dcecb80c8baee78ecd518ce37e9d
harsha6769 <harshavardhanreddygopireddy1@gmail.com>
2026-06-20 19:01:06 +0530

    Update backend API URL

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,15 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'https://hirehub-ats-3dqk.onrender.com',
         changeOrigin: true,
-        secure: false,
+        secure: true
       }
     }
   }
